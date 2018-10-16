7d70b0ff8ca8e10d55178e71502382d908fbb95e
unknown <samiul.saad@gmail.com>
2018-10-16 15:33:02 +0600

    edited

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Portfolio from './component/Portfolio/portfolio';
 import Offer from './component/Offer/offer';
 import Team from './component/Team/team';
 import Footer from './component/Footer/footer';
-import ScrollToTop from 'react-scroll-up'
+
 import {Button } from 'react-bootstrap'
 
 
@@ -28,9 +28,7 @@ class App extends Component {
         <Team/>
         <Footer/>
 
-        <ScrollToTop showUnder={120} topPosition={0} >
-         <Button bsStyle="info"> <span> UP</span></Button>
-        </ScrollToTop>
+       
 
 
 

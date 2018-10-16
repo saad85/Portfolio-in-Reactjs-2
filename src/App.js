b795bde5e73187ad10cddc0ebaf1145d918e5542
unknown <samiul.saad@gmail.com>
2018-10-16 13:13:44 +0600

    edited

--- src/App.js
+++ src/App.js
@@ -7,18 +7,33 @@ import Portfolio from './component/Portfolio/portfolio';
 import Offer from './component/Offer/offer';
 import Team from './component/Team/team';
 import Footer from './component/Footer/footer';
+import ScrollToTop from 'react-scroll-up'
+import {Button } from 'react-bootstrap'
+
+import {Route,
+  NavLink,
+  Router} from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+
         <Header/>
         <Feature/>
         <Portfolio/>
         <Offer/>
         <Team/>
         <Footer/>
+
+        <ScrollToTop showUnder={120} topPosition={0} >
+         <Button bsStyle="info"> <span> UP</span></Button>
+        </ScrollToTop>
+
+
+
       </div>
     );
   }
--- src/component/Navbar/navbar.js
+++ src/component/Navbar/navbar.js
@@ -9,20 +9,20 @@ class Navigationbar extends React.Component{
         
                     <div>
                         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                            <a class="navbar-brand" href="#">SAMIUL</a>
+                            <a class="navbar-brand"  href="#">SAMIUL</a>
                             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
                             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                                 <ul class="navbar-nav navbar-ul">
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link" to="/home" href="#">Home <span class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Feature</a>
+                                    <a class="nav-link" to="/feature" href="#">Feature</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Portfolio</a>
+                                    <a class="nav-link" to="/portfolio" href="#">Portfolio</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">Offer</a>
